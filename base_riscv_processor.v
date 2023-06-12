6c7e878eae521dc301b6deb8a1bb11b8704947cf
CarlosEngler <carlos.engler@usp.br>
2023-06-11 23:15:42 -0300

    🚀 add Load and Store to UC

--- base_riscv_processor.v
+++ base_riscv_processor.v
@@ -84,11 +84,13 @@ module InstructionMemory (
   reg [31:0] mem [31:0];    
    
     always @(posedge clk) begin
-      mem[0] <= 32'b00000000000000000 011 00001 0000011; //l-format Load:
-      mem[1] <= 32'b00000000000000000 011 00010 0000011; //load X2<=mem(1 + 0)
-      //mem[1] <= 32'b00000000000000000011000000100011; //s-format Store:
+      //mem[0] <= 32'b00000000000000000011000010000011; //l-format Load: mem(0) => X1
+      //mem[1] <= 32'b00000000000000010011000010000011; //load 
+
+      mem[0] <= 32'b00000000000000000011000010100011; //s-format Store: X0 -> mem(0+1)
+      
       //mem[2] <= 32'b00000000010000010000001100110011;
-        mem[3] <= 32'b00000000000100010000001100110011;   //r-format Add: X6 <= X2 + X4
+      mem[3] <= 32'b00000000000100010000001100110011;   //r-format Add: X6 <= X2 + X4
       //mem[3] <= 32'b01000000001100110000001110110011; //r-format Sub: X7 <= X6 - X3
       //mem[4] <= 32'b00000000100100010000000010010011; //l-format Addi: X1 <= X2 + 9
       //mem[5] <= 32'b00000000101000011000001010010011; //l-format Subi: X5 <= X3 - 10 nota: nossa ula inverte o número passado pra ela dentro de seu próprio funcionamento, portanto passamos o valor do immediate em módulo.
@@ -287,6 +289,7 @@ module datapath(
   output BLTU,
   output [63:0] douta_saida,
   output [63:0] data_ram,
+  output [63:0] dout_ULA,
   output [31:0] saida_IR,
   output [63:0] saida_PC,
   output [31:0] saida_MI
@@ -353,6 +356,7 @@ module datapath(
 
   assign douta_saida = douta;
   assign data_ram = dout_ram;
+  assign dout_ULA = saida_ula;
 
 endmodule
 
@@ -395,15 +399,20 @@ module uc(
   initial begin
     we_pc = 1;
     PCres = 64'd0;
+    decisor0 = 0;
+    decisor1 = 0;
+    decisor2 = 0;
     decisor3 = 0;
     decisor4 = 0;
+    decisor5 = 0;
+    decisor6 = 0;
+    somador_subtrator = 0;
     sel = 2'b00;
     reset_PC = 1'b1;
     we = 0; 
     we_ir = 0;
     we_pc = 0;
 
-    decisor3 <= 1;
     
    
   end
@@ -423,6 +432,7 @@ module uc(
         we_pc = 1;
         we_ir = 1;
         sel <= 2'b01;
+
       end
 
       2'b01: //decode
@@ -439,6 +449,12 @@ module uc(
         begin
           sel<= 2'b10;
           selEx <= 3'b001;
+        end
+
+        if(saida_MI[6:0] == 7'b0100011)//store
+        begin
+          sel<= 2'b10;
+          selEx <= 3'b010; 
         end
 
       end
@@ -478,13 +494,28 @@ module uc(
            decisor2 = 1;
            decisor5 = 1;
 
-           Ra = saida_MI[11:7]; //setta o endereço da ram que será loadado        
-           Rw = saida_MI[19:15]; //setta o endereço do rf que será loada
+           Ra = saida_MI[19:15]; //setta o endereço da ram que será loadado        
+           Rw = saida_MI[11:7]; //setta o endereço do rf que será loada
            entrada_mux_add_sub = saida_MI[31:20]; //imeddiate
           
            we = 1; 
            sel <= 2'b11;
            end
+           
+          3'b010: //store
+          begin
+          Ra = saida_MI[25:20]; //setta o endereço do RF
+          Rb = saida_MI[19:15]; //setta o endereço da Ram
+          entrada_mux_add_sub = {saida_MI[31:25], saida_MI[11:7]}; //setta o que vai ser somado ao endereço que acessará a Ram;
+
+          //ajuste dos MUX
+          decisor0 = 1;
+          decisor1 = 0;
+
+          we_ram = 1;
+
+          sel = 2'b11;
+          end
 
           3'b011: 
           begin
@@ -509,9 +540,7 @@ module uc(
 
       2'b11: //write back
       begin
-        we = 0;
         Ra = Rw;
-        we_ram = 0;
         //sel = 2'b00;
       end
 
@@ -545,6 +574,7 @@ module testbench2_tb();
   wire decisor_somador_subtrator;
   wire [63:0] douta_saida; //contém conteúdo do registrador acessado por Ra
   wire [63:0] data_ram; //contém conteúdo do elemento de memória acessado pela saída da ULA, que é o resultado da soma
+  wire [63:0] dout_ULA;
   wire [31:0] saida_IR;
   wire [31:0] saida_MI;
   wire [63:0] saida_PC;
@@ -555,6 +585,7 @@ module testbench2_tb();
   wire BLT;
   wire BGEU;
   wire BLTU;
+
    
 
   uc auc(
@@ -605,16 +636,20 @@ module testbench2_tb();
   .BEQ(BEQ), .BGE(BGE), .BNE(BNE), .BLT(BLT),
   .BGEU(BGEU), .BLTU(BLTU), .saida_MI(saida_MI),
   .somador_PC(somador_PC), .saida_PC(saida_PC),
-  .imm_PC(imm_PC)
+  .imm_PC(imm_PC),
+  .dout_ULA(dout_ULA)
 );
 
 initial begin
   $dumpfile("test.vcd");
     $dumpvars(0);
- //32'b0000000 00100 00010 000 00110 0110011; //r-format Add: X6 <= X2 + X4 mem(0)
+
     clk = 1'b0;
+    //load = 60
+    //store = 50
+
     
-    #10 $display("valor de X6 após add (X6 <= X4 + X2) = %d", douta_saida);
+    #50 $display(" (X0 -> mem(0+1)) = %d", data_ram);
 
     $stop;
   

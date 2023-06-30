dabf19cedad0f116cf58bedae31bd9c5d8bd926e
JPBF10 <joaopb7f10@gmail.com>
2023-06-30 09:54:45 -0300

    :sparkles: inicio multiplicaco e descrico do que fazer

--- FP/Datapath.v
+++ FP/Datapath.v
@@ -339,6 +339,7 @@ module testbench;
     reg subtrador_big_ula;
     reg subtrador_Somador_subtrador;
     reg boolean;
+    reg operecao;
     wire overflow; 
     wire directionShift;
     wire [7:0] tamanhoShift;
@@ -379,87 +380,110 @@ module testbench;
       
       clk = 1'b0;
       load = 1'b0;
+      operecao = 1'b0; // soma = 1, multiplicao = 0
 
-      //set os inputs
-      input_1 = 32'b01000001100011000000000010000000;
-      input_2 = 32'b01000001000000000001010000000100;
-      // zero-multiplica, um-soma
-      soma_multiplica_small_ula = 1'b1;
-      soma_multiplica_big_ula = 1'b1;
 
-      #10;
-      $display("saida registrador", saida_registrador);
+      if(operecao) begin //soma
+        //set os inputs
+        input_1 = 32'b01000001100011000000000010000000;
+        input_2 = 32'b01000001000000000001010000000100;
 
-      //faz a equcao da big ula
+        //inicio
+        soma_multiplica_small_ula = 1'b1; // zero-multiplica, um-soma
+        soma_multiplica_big_ula = 1'b1; // zero-multiplica, um-soma
+
         #10;
-        //zero-soma, um-subtrai
-        subtrador_big_ula = 1'b0;
-        tamanho = saida_registrador;
-        
-        #10;
-        $display("saida big ula %b", data_out_big_ula);
-      //pos operecao big_ula
+        $display("saida registrador", saida_registrador);
 
-        //normalizacao geral
-        #10;
-          decisor_mux_expoente_escolhido = 1'b0;
-          subtrador_Somador_subtrador = 1'b0; //0:soma , 1:subtrai
-          tamanho3 = saida_registrador;
+        //faz a equacao da big ula
           #10;
-          load = 1'b1;
-          #10;
-          load = 1'b0;
-
-          #10;
-          //mux da esquerda
-              tamanho3 = tamanhoShift - directionShift;
-              tamanho2 = tamanhoShift[4:0] - directionShift;
+          subtrador_big_ula = 1'b0; //zero-soma, um-subtrai
+          tamanho = saida_registrador;
           
           #10;
-          decisor_mux_expoente_escolhido = 1'b1;
-          subtrador_Somador_subtrador = 1'b1; //0:soma , 1:subtrai
+          $display("saida big ula %b", data_out_big_ula);
+        //pos operecao big_ula
 
-          // mux da direita
-          decisor_mux_saida_big_ula = 1'b0; //0 = big ula, 1 = arredondamento
-          decisor_shift_right_left = directionShift;
+          //normalizacao geral
+          #10;
+            decisor_mux_expoente_escolhido = 1'b0;
+            subtrador_Somador_subtrador = 1'b0; //0:soma , 1:subtrai
+            tamanho3 = saida_registrador;
+            #10;
+            load = 1'b1;
+            #10;
+            load = 1'b0;
 
-          #10;
-          load = 1'b1;
-          #10;
-          load = 1'b0;
+            #10;
+            //mux da esquerda
+                tamanho3 = tamanhoShift - directionShift;
+                tamanho2 = tamanhoShift[4:0] - directionShift;
+            
+            #10;
+            decisor_mux_expoente_escolhido = 1'b1;
+            subtrador_Somador_subtrador = 1'b1; //0:soma , 1:subtrai
+
+            // mux da direita
+            decisor_mux_saida_big_ula = 1'b0; //0 = big ula, 1 = arredondamento
+            decisor_shift_right_left = directionShift;
+
+            #10;
+            load = 1'b1;
+            #10;
+            load = 1'b0;
+            
+            #10; // gambiarra monstruosa, dando shift right, muitos casos deram certo
+            subtrador_Somador_subtrador = 1'b0; //0:soma , 1:subtrai
+            tamanho3 = 8'd1;
+            tamanho2 = 5'd1;
+            decisor_shift_right_left = 1'b0;    // 1 = shift left, 0 = shift right
+            
+            #10;
+            load = 1'b1;
+            #10;
+            load = 1'b0;
+
+
+            //caso overflow no rounding -> joga o número original pro shift_right_left e da um shift right, consequentemente soma 1 no expoente
+            if(overflow)
+            begin
+            decisor_mux_saida_big_ula = 1'b1; //shift right left recebe fração do arredonda
+            decisor_shift_right_left = 1'b0; //shift right
+            tamanho2 = 5'd1;
+
+
+            decisor_mux_expoente_escolhido = 1'b1; //inc dec recebe expoente arredondado
+            subtrador_Somador_subtrador = 1'b0; //0:soma , 1:subtrai
+            tamanho3 = 8'd1; //soma 1 no expoente
+
+            #10;
+            load = 1'b1;
+            #10;
+            load = 1'b0;
+            end
+        end
+
+        else begin //multiplicao
+
+          //set os inputs
+          input_1 = 32'b01000001100011000000000010000000;
+          input_2 = 32'b01000001000000000001010000000100;
           
-          #10; // gambiarra monstruosa, dando shift right, muitos casos deram certo
-          subtrador_Somador_subtrador = 1'b0; //0:soma , 1:subtrai
-          tamanho3 = 8'd1;
-          tamanho2 = 5'd1;
-          decisor_shift_right_left = 1'b0;    // 1 = shift left, 0 = shift right
+          //inicio
+          soma_multiplica_small_ula = 1'b0; // zero-multiplica, um-soma
+          soma_multiplica_big_ula = 1'b0; // zero-multiplica, um-soma
+
+          #10;
+        $display("saida registrador", saida_registrador);
+
+        //faz a equacao da big ula
+          #10;
+          tamanho = saida_registrador;
           
           #10;
-          load = 1'b1;
-          #10;
-          load = 1'b0;
+          $display("saida big ula %b", data_out_big_ula);
 
-
-          //caso overflow no rounding -> joga o número original pro shift_right_left e da um shift right, consequentemente soma 1 no expoente
-          if(overflow)
-          begin
-          decisor_mux_saida_big_ula = 1'b1; //shift right left recebe fração do arredonda
-          decisor_shift_right_left = 1'b0; //shift right
-          tamanho2 = 5'd1;
-
-
-          decisor_mux_expoente_escolhido = 1'b1; //inc dec recebe expoente arredondado
-          subtrador_Somador_subtrador = 1'b0; //0:soma , 1:subtrai
-          tamanho3 = 8'd1; //soma 1 no expoente
-
-          #10;
-          load = 1'b1;
-          #10;
-          load = 1'b0;
-          end
-
-
-          //multiplicação
+        end
 
 
         #10;
@@ -473,8 +497,6 @@ module testbench;
 
     always #5 clk= ~clk;
 endmodule
-//teste 01000001100011000000000000000000
-//teste 01000001100111000000000000000000
 
 
-// PROBLEMAS: ajustar o signal na multiplicação, 
+// PROBLEMAS: ajustar o signal na multiplicação,  -> fazer isso na uc, é apenas um shor
